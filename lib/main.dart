197f505005bbd7e64576c0064b21269a4ff0534b
snt-nbzw <sntnbzw@gmail.com>
2020-11-23 17:38:42 +0900

    185. Refactor Routes with the Static Const

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
+import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
@@ -24,9 +25,9 @@ class FlashChat extends StatelessWidget {
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'registrarion_screen': (context) => RegistrationScreen(),
-        'chat_screen': (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
